307cd7e02c26c34c2c576d90e431bc1477f5e500
MoonBooster <glenerikcortes@gmail.com>
2018-05-02 18:59:47 -0400

    Update neataptic_LSTM.js

--- neataptic_LSTM.js
+++ neataptic_LSTM.js
@@ -9,6 +9,8 @@ const strat = {
     config.silent = true;
     config.debug = false;
     this.addIndicator('neat', 'NEAT_LSTM', {
+      nn_log: this.settings.log,
+      skip_training: this.settings.skip_training,
       batchSize: this.settings.batchSize,
       hiddenLayers: this.settings.hiddenLayers,
       lookAhead: this.settings.lookAheadCandles,
@@ -35,24 +37,27 @@ const strat = {
     if (!this.indicators.neat.prediction){
       console.log("NO PREDICTION");
     } else {
-      const low_prediction =  this.indicators.neat.prediction[0].toFixed(8),
-            high_prediction = this.indicators.neat.prediction[1].toFixed(8);
-      console.log(candle.start.format(), 
-      " Predicted \t LOW: ", low_prediction,
-      " | HIGH: ", high_prediction,
-      " | Current Price \t LOW: ", candle.low,
-      " | HIGH: ", candle.high);
-      const short = candle.close*(1 - this.settings.short_at_percent) > low_prediction ? true : false,
-            long = candle.close*(1 + this.settings.long_at_percent) < high_prediction ? true : false;
+      const short = candle.close*(1 - this.settings.short_at_percent) > this.indicators.neat.prediction[0].toFixed(8) ? true : false,
+            long = candle.close*(1 + this.settings.long_at_percent) < this.indicators.neat.prediction[1].toFixed(8) ? true : false;
 
       if (short && this.last_action !== 'sell') {
         this.advice('short');
+        this.print_to_console(candle, 'short');
       } else 
       if (long && this.last_action !== 'buy') {
         this.advice('long');
+        this.print_to_console(candle, 'long');
       }
     }
 
+  },
+  print_to_console(candle, advice) {
+    console.log(candle.start.format(), 
+    " Predicted \t LOW: ", this.indicators.neat.prediction[0].toFixed(8),
+    " | HIGH: ", this.indicators.neat.prediction[1].toFixed(8),
+    " | Current Price \t LOW: ", candle.low,
+    " | HIGH: ", candle.high, 
+    " | ADVICE: ", advice);
   },
 
   convertToPercent(numerator, denominator) {
@@ -71,9 +76,9 @@ const strat = {
     log.info('=======================');
   },
   onTrade (event){
-    log.debug(event.date.format(), event + ' at: ', event.price.toFixed(8));
-    this.trend.last_action = event.action;
-    this.trend.action_price = event.price;
+    log.debug(event.date.format(), event.action + ' at: ', event.price.toFixed(8));
+    this.last_action = event.action;
+    this.action_price = event.price;
 }
 }
 
